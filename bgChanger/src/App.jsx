caf8859ecc7b27c8f0c8dc1134203984ac8523f9
Harshil Patel <harshilp046@gmail.com>
2024-05-07 23:25:42 +0530

    Fixed when background and displaying text is same color

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
-    <div className="text-center pt-24 text-5xl font-bold underline">
+    <div className="text-center pt-24 text-5xl font-bold underline" style={{color: color === 'Black' ? 'white' : 'black'}}>
       Current Displaying Color: {color}
     </div>
       <div className="fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-2">
